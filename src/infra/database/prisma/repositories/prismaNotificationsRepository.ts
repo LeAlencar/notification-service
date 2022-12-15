87b4dc15dd0dd86ca60f857759823de516b4d064
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-12-14 21:03:02 -0300

    feat(notification): Created a new mapper

--- src/infra/database/prisma/repositories/prismaNotificationsRepository.ts
+++ src/infra/database/prisma/repositories/prismaNotificationsRepository.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { Notification } from '@application/entities/notification';
 import { NotificationsRepository } from '@application/repositories/notificationRepository';
 import { PrismaService } from '../prisma.service';
+import { PrismaNotificationMapper } from '../mappers/prismaNotificationMapper';
 
 @Injectable()
 export class PrismaNotificationRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        content: notification.content.value,
-        category: notification.category,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
